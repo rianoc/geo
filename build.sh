8e16178ad85f138f81c89e03249f426966a91e6b
Rian Ó Cuinneagáin <rocuinneagain@gmail.com>
2019-02-18 16:21:17 +0000

    Pull latest k.h. Do not fully rebuild s2

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash -e
 mkdir -p build
-cp -r q s2 build/
-cp Makefile k.h k.cc build/s2/
-find build -name "*.o" -exec rm -rf {} \;
-find build -name "*.so" -exec rm -rf {} \;
+if [ ! -e build/s2 ]; then
+ cp -r s2 build/ 
+fi
+cp Makefile k.cc build/s2/
+if [ ! -f build/s2/k.h ]; then
+ wget -O build/s2/k.h https://raw.githubusercontent.com/KxSystems/kdb/master/c/c/k.h
+fi
 cd build/s2
+rm -f s2.so k.o
 make
 
